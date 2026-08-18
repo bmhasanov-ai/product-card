852306fe7b94b83825ea8ae07a649a62cf4b6ae8
Khasanov Bulat <bmhasanov@gmail.com>
2026-08-18 20:42:09 +0300

    fix function/const names

--- homework-9.js
+++ homework-9.js
@@ -29,34 +29,34 @@ reverseArray(films);
 
 // Задание 7.
 
-const comEmails = comments.filter(comment => comment.email.includes(".com"));
+const getComEmails = comments.filter(comment => comment.email.includes(".com"));
 
 // Задание 8.
 
-const newIdComments = comments.map(comment => { comment.id <= 5 ? comment.postId = 2 : comment.postId = 1; return comment; })
+const getNewIdComments = comments.map(comment => { comment.id <= 5 ? comment.postId = 2 : comment.postId = 1; return comment; })
 
 
 // Задание 9.
 
-const shortComments = comments.map(comment => {
+const getShortComments = comments.map(comment => {
   return { id: comment.id, name: comment.name }
 })
 
 // Задание 10.
 
-const newComments = comments.map(comment => { comment.body.length > 180 ? comment.isInvalid = true : comment.isInvalid = false; return comment; })
+const getNewComments = comments.map(comment => { comment.body.length > 180 ? comment.isInvalid = true : comment.isInvalid = false; return comment; })
 
 // Задание 11.
 
-const emailsArray = comments.reduce((acc, comment) => {
+const emailsByReduce = comments.reduce((acc, comment) => {
   acc.push(comment.email);
   return acc;
 }, []);
 
-const emailsArray2 = comments.map(comment => comment.email);
+const emailsByMap = comments.map(comment => comment.email);
 
 // Задание 12.
 
-const stringComments = emailsArray.join(" | ");
-const stringComments2 = emailsArray.toString();
+const stringComments = emailsByReduce.join(" | ");
+const stringComments2 = emailsByMap.toString();
 
