e0a6a4098fce1ebf0e97e67663121b5e4a8f01e8
Khasanov Bulat <bmhasanov@gmail.com>
2026-08-25 15:39:42 +0300

    fix next msitakes

--- homework-9.js
+++ homework-9.js
@@ -29,13 +29,9 @@ reverseArray(films);
 
 // Задание 7.
 
-const ComEmails = comments.filter(comment => comment.email.includes(".com"));
+const comEmails = comments.filter(comment => comment.email.includes(".com"));
 
 // Задание 8.
-
-const NewIdComments = comments.map(comment => { comment.id <= 5 ? comment.postId = 2 : comment.postId = 1; return comment; })
-
-console.log(NewIdComments);
 
 const newComments = comments.map(comment => {
   return {
@@ -44,16 +40,15 @@ const newComments = comments.map(comment => {
   };
 });
 
-console.log(newComments);
 // Задание 9.
 
-const ShortComments = comments.map(comment => {
+const shortComments = comments.map(comment => {
   return { id: comment.id, name: comment.name }
 })
 
 // Задание 10.
 
-const NewComments = comments.map(comment => { comment.body.length > 180 ? comment.isInvalid = true : comment.isInvalid = false; return comment; })
+const newComments = comments.map(comment => { comment.body.length > 180 ? comment.isInvalid = true : comment.isInvalid = false; return comment; })
 
 // Задание 11.
 
